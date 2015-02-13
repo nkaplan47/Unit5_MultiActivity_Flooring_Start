3a2fc1e39ade5f832f3d824d885ba1f88f13e2d4
nkaplan <nkaplan@css.edu>
2015-02-13 13:47:48 -0600

    Created the new activity

--- app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/MainActivity.java
+++ app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/MainActivity.java
@@ -1,18 +1,33 @@
 package edu.css.cis3334.unit5_multiactivity_flooring_start;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText txtWidth;
+    EditText txtLength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtWidth = (EditText) findViewById(R.id.etWidth);
+        txtLength = (EditText) findViewById(R.id.etLength);
     }
+
+    public void onClick(View v){
+        Intent i = new Intent(this, ResultsActivity.class);
+        //i.putExtra("Width is " + txtWidth.toString() + "and Length is " + txtLength.toString() + " and flooring needed is ");
+        startActivity(i);
+    }
+
 
 
     @Override
